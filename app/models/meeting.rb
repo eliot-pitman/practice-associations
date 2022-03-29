8e68561665fd10333725eb5ee284a984abaa1d4d
Eliot <eliotpitman@gmail.com>
2022-03-29 18:31:58 -0500

    adds todays homeowrk

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,18 @@
 class Meeting < ApplicationRecord
+  require 'date'
+
   has_many :meeting_speakers
   has_many :speakers, through: :meeting_speakers
+
+
+  # date = Date.parse("#{time}")
+  # formatted_date = date.strftime('%a %b %d %H:%M:%S %Z %Y')
+
+  # validate :meeting_date_cannot_be_in_the_past
+  # def meeting_date_cannot_be_in_the_past
+  #   if date.present? && date < Date.today
+  #     errors.add(:expiration_date, "can't be in the past")
+  #   end
+  # end
+
 end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,4 +1,6 @@
 class Speaker < ApplicationRecord
   has_many :meeting_speakers
   has_many :meetings, through: :meeting_speakers
+
+  validates :speaker, :presence: true
 end
