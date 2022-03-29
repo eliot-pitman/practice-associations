481a043bc611bc584d20aef913eeb750c1546a2a
Eliot <eliotpitman@gmail.com>
2022-03-29 17:54:48 -0500

    adds database, and association

--- db/migrate/20220329223536_create_speakers.rb
+++ db/migrate/20220329223536_create_speakers.rb
@@ -1,7 +1,9 @@
 class CreateSpeakers < ActiveRecord::Migration[7.0]
   def change
     create_table :speakers do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
+      t.string :email
 
       t.timestamps
     end
--- db/migrate/20220329223553_create_meetings.rb
+++ db/migrate/20220329223553_create_meetings.rb
@@ -1,7 +1,10 @@
 class CreateMeetings < ActiveRecord::Migration[7.0]
   def change
     create_table :meetings do |t|
-      t.string :topic
+      t.string :title
+      t.string :agenda
+      t.string :location
+      t.string :time
 
       t.timestamps
     end
